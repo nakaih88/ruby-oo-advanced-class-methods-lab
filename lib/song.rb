7ab1d12b2f1c82aa7475c7a42c20edbf013b2175
Nakai <nakai.hamlin18@gmail.com>
2021-03-05 17:22:48 -0500

    commit tree 3ddd8f26fe1b09639957caff7ad369b1d69329bc

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,53 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    self.all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect {|song| song.name === name}
+  end
+
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|index| index.name}
+  end
+  
+  def self.new_from_filename(file)
+    artist = file.split("-")[0].strip
+    song = file.split("-")[1].split(".")[0].strip
+    new_song = Song.new
+    new_song.name = song
+    new_song.artist_name = artist
+    new_song
+  end
+
+  def self.create_from_filename(file)
+    new_song = self.new_from_filename(file)
+    new_song.save
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 end
